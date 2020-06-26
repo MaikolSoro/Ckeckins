e1cba74181eb26dd5993acccfce23a798b13e102
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-26 16:24:17 -0600

    Continuando con los metodos para mostrar los datos

--- app/src/main/java/com/example/ckeckins/RecyclerViewPrincipal/AdaptadorCustom.kt
+++ app/src/main/java/com/example/ckeckins/RecyclerViewPrincipal/AdaptadorCustom.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ckeckins.R
 import com.example.ckeckins.Venue
-import java.text.FieldPosition
 
 class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var longClickListener: LongClickListener) {
 
@@ -16,11 +15,83 @@ class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var l
     var multiSeleccion = false
 
     var itemsSeleccionados: ArrayList<Int>? = null
-    var viewHolder: RecyclerView.ViewHolder? = null
+    var viewHolder: ViewHolder? = null
 
     init {
         this.items = items
         itemsSeleccionados = ArrayList()
+    }
+
+
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
+     * an item.
+     *
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * [.onBindViewHolder]. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see .getItemViewType
+     * @see .onBindViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val vista = LayoutInflater.from(parent?.context).inflate(R.layout.template__venues, parent, false)
+            viewHolder = ViewHolder(vista, listener, longClickListener)
+            return viewHolder!!
+    }
+
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+        override fun getItemCount(): Int {
+            return items?.count()!!
+        }
+     */
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
+     * position.
+     *
+     *
+     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the `position` parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
+     * have the updated adapter position.
+     *
+     * Override [.onBindViewHolder] instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items?.get(position)
+        holder.nombre?.text = item?.name
+        if (itemsSeleccionados?.contains(position)!!) {
+            holder.vista.setBackgroundColor(Color.LTGRAY)
+
+        } else {
+            holder.vista.setBackgroundColor(Color.WHITE)
+        }
     }
 
     /*
@@ -44,7 +115,9 @@ class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var l
             holder.vista.setBackgroundColor(Color.WHITE)
         }
     }
-  */
+    */
+
+     */
     fun iniciarActionMode() {
         multiSeleccion = true
     }
@@ -52,7 +125,7 @@ class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var l
     fun destruirActionMode() {
         multiSeleccion = false
         itemsSeleccionados?.clear()
-        //notifyDataSetChanged()
+       //notifyDataSetChanged()
     }
     fun terminarActionMode() {
         for (item in itemsSeleccionados!!){
@@ -69,22 +142,22 @@ class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var l
             } else {
                 itemsSeleccionados?.add(index)
             }
-          //  notifyDataSetChanged()
+            //notifyDataSetChanged()
         }
     }
-    /*
-    fun obtenerNumeroElementosSeleccionados(): Int {
-      //  return itemsSeleccionados?.count()
-    }
-    */
+
+   // fun obtenerNumeroElementosSeleccionados(): Int {
+        //return itemsSeleccionados?.count()
+    //}
+
 
 
     fun eliminarSeleccionados() {
         if(itemsSeleccionados?.count()!! > 0) {
-            var itemsEliminados = ArrayList<Venue>()
+            var itemsEliminados = ArrayList<Venue?>()
 
             for(index in itemsEliminados!!) {
-              //  itemsEliminados.add(items?.get(index)!!)
+               // itemsEliminados.add(items?.get(index)!!)
             }
             items?.removeAll(itemsEliminados)
             itemsSeleccionados?.clear()
@@ -103,9 +176,11 @@ class AdaptadorCustom(item: ArrayList<Venue>, var listener: ClickListener, var l
             this.listener = listener
             this.longListener = longClickListener
 
-            //vista.setOnClickListener(this)
-            //vista.setOnLongClickListener(this)
+            ///vista.setOnClickListener(this)
+            // vista.setOnLongClickListener(this)s
         }
+
+
 
         /*
            override fun OnClick(v: View?) {
